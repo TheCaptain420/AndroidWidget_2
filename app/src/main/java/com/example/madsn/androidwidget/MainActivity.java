449123f8fc2cdb60782637b89dc6bef0c46241cf
thecaptain420 <mads130f@edu.easj.dk>
2019-01-14 22:42:03 +0100

    Widget kan nu display beskeder jeg har skrevet, og den kan trykkes på :D

--- app/src/main/java/com/example/madsn/androidwidget/MainActivity.java
+++ app/src/main/java/com/example/madsn/androidwidget/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.madsn.androidwidget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.paperdb.Paper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSave;
+    EditText edtTitle,edtContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //init paper
+        Paper.init(this);
+
+        btnSave = (Button)findViewById(R.id.btn_save);
+        edtTitle = (EditText)findViewById(R.id.edt_title);
+        edtContent = (EditText) findViewById(R.id.edt_content);
+
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Paper.book().write("title",edtTitle.getText().toString());
+                Paper.book().write("content",edtContent.getText().toString());
+
+            }
+        });
     }
 }
