73729cb71eadee89f504c0addfdb14e25d5ad138
thecaptain420 <mads130f@edu.easj.dk>
2019-01-22 22:38:07 +0100

    Appen kan nu tænde samt slukke wifi på telefonen.. :D

--- app/src/main/java/com/example/madsn/androidwidget/MainActivity.java
+++ app/src/main/java/com/example/madsn/androidwidget/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.madsn.androidwidget;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnSave;
     EditText edtTitle,edtContent;
+
+    //wifi
+    Button btn_wifiOn, btn_wifiOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,10 @@ public class MainActivity extends AppCompatActivity {
         edtTitle = (EditText)findViewById(R.id.edt_title);
         edtContent = (EditText) findViewById(R.id.edt_content);
 
+        btn_wifiOn = (Button)findViewById(R.id.wifi_on);
+        btn_wifiOff = (Button)findViewById(R.id.wifi_off);
+
+
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +44,32 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        btn_wifiOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("tsm button pressed wifi On");
+                //<uses-permission android:name="android.permission.CHANGE_WIFI_STATE" />
+
+                WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                wifi.setWifiEnabled(true); // true or false to activate/deactivate wifi
+
+            }
+        });
+
+        btn_wifiOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("tsm button pressed wifi off");
+
+                WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                wifi.setWifiEnabled(false); // true or false to activate/deactivate wifi
+            }
+        });
+
+
+
+
     }
 }
